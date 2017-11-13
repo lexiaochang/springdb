6f3ed4bd6d5206c47a968fe29e61453d8a11d1a4
ChrisLeGitHub <mrchriscl@icloud.com>
2017-11-13 17:59:41 +0800

    添加连接数据库注释

--- src/main/java/com/chris/springdb/controller/ConnecrDBController.java
+++ src/main/java/com/chris/springdb/controller/ConnecrDBController.java
@@ -1,11 +1,16 @@
 package com.chris.springdb.controller;
 
 import com.chris.springdb.service.PersonService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -25,10 +30,19 @@ public class ConnecrDBController {
         return "success!";
     }
 
-    @RequestMapping(value = "v1.0", method = RequestMethod.GET)
+    @RequestMapping(value = "v1.0", method = RequestMethod.POST)
     @ResponseBody
-    public String test1() {
-        return "successfully";
+    public ResponseEntity<String> connectdb01(@RequestParam String name, @RequestParam String password) throws JsonProcessingException {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("success", password);
+            json.put("message", name);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        ResponseEntity<String> re = new ResponseEntity<>(json.toString(), HttpStatus.OK);
+        return re;
     }
 
 }
